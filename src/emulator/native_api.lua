73149fd58feb0c06f666eaf015774bec941f536a
jesusthekiller <stuff@minebox.xorg.pl>
2014-02-22 20:41:02 +0100

    Added "mailto:" and "file:" to custom msg, Fix

--- src/emulator/native_api.lua
+++ src/emulator/native_api.lua
@@ -439,9 +439,9 @@ function NativeAPI:initialize(_computer)
 		sUrl = sUrl:match'^%s*(.*%S)' or ''
 		
 		--# Assert that sUrl is now not ""
-		api_assert(#sUrl < 0, "Invalid URL")
+		api_assert(#sUrl <= 0, "Invalid URL")
 		
-		api_assert(sUrl:sub(1, 4) =~ "ftp:", "Not an HTTP URL") -- Any others that report this error?
+		api_assert(sUrl:sub(1, 4) =~ "ftp:" and sUrl:sub(1, 7) =~ "mailto:", sUrl:sub(1, 5) ~= "file:", "Not an HTTP URL") -- Any others that report this error?
 		api_assert(sUrl:sub(1, 5) == "http:" or sUrl:sub(1, 6) == "https:", "Invalid URL")
 		
 		local http = HttpRequest.new()
